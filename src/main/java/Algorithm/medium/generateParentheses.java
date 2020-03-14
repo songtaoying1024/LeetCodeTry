79d1f8cf7ffc6c3932443aecfd3c5d1c63d17e97
Taoying <t.song@qimia.de>
2020-03-14 16:08:48 +0100

    ha

---
+++ src/main/java/Algorithm/medium/generateParentheses.java
@@ -0,0 +1,28 @@
+package Algorithm.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * For example, given n = 3, a solution set is:
+ *
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ */
+public class generateParentheses {
+    public List<String> generateParenthesis(int n){
+        ArrayList<String> str = new ArrayList<>();return str;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
