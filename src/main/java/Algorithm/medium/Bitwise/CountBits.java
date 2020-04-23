7cdcabfccaf5863b0d8f9a50d32d4d86ae9ba43e
Taoying <t.song@qimia.de>
2020-04-23 17:59:00 +0200

    bitwise

--- src/main/java/Algorithm/medium/Bitwise/CountBits.java
+++ src/main/java/Algorithm/medium/Bitwise/CountBits.java
@@ -1,4 +1,4 @@
-package Algorithm.medium;
+package Algorithm.medium.Bitwise;
 
 /**
  * Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
@@ -16,12 +16,15 @@ package Algorithm.medium;
  * It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
  * Space complexity should be O(n).
  * Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+ *
  */
 public class CountBits {
     public int[] countBits(int num) {
         int[] f = new int[num + 1];
         for (int i=1; i<=num; i++)
         {
+            /** >> 二进制往右移一位*/
+            /** &是一个二进制与运算*/
             f[i] = f[i>>1]+(i&1);
         }
         return f;
---
+++ src/main/java/Algorithm/medium/Bitwise/Numberof1Bites.java
@@ -0,0 +1,43 @@
+package Algorithm.medium.Bitwise;
+
+/**
+ * Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: 00000000000000000000000000001011
+ * Output: 3
+ * Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+ * Example 2:
+ *
+ * Input: 00000000000000000000000010000000
+ * Output: 1
+ * Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+ * Example 3:
+ *
+ * Input: 11111111111111111111111111111101
+ * Output: 31
+ * Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+ *
+ */
+public class Numberof1Bites {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count =0;
+        while(n!=0){
+            count+=n&1;
+            /** >>> is unsigned shift */
+            /**https://stackoverflow.com/questions/19058859/what-does-mean-in-java */
+            n>>>=1;
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((Integer.MAX_VALUE+1)>>>1);
+    }
+
+}
